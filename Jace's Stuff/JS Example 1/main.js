f7047736030c825e18e92b64d1674ca1774d826b
FloorIsGround <112002787+FloorIsGround@users.noreply.github.com>
2022-08-31 12:10:44 -0500

    debug examples

--- Jace's Stuff/JS Example 1/main.js
+++ Jace's Stuff/JS Example 1/main.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let debug = true;
     function createParagraph() {
       const para = document.createElement('p');
       para.textContent = 'You clicked the button!';
       document.body.appendChild(para);
+      if(debug == true){
+        console.log("New line created.");
+      }
     }
   
     const buttons = document.querySelectorAll('button');
--- Jace's Stuff/JS Example 1/playernamechange.js
+++ Jace's Stuff/JS Example 1/playernamechange.js
@@ -1,7 +1,12 @@
 const para = document.querySelector('p');
         para.addEventListener('click', updateName);
-
+        let debug = true;
         function updateName() {
             let name = prompt('Enter the user name');
+            if(debug == true){
+                console.log("User prompted for name change.");
+                console.log("User changed name to " + name + ".");
+            }
             para.textContent = "Player 1: " + name
+
         }
