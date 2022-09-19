35fcca8f9539041ce89da9e236d2c4d405c43a60
FloorIsGround <112002787+FloorIsGround@users.noreply.github.com>
2022-09-19 12:44:37 -0500

    closure with a recursion example
    recursion of an array of any size to read out all elements of an array

--- Jace's Stuff/JS Example 1/closure.js
+++ Jace's Stuff/JS Example 1/closure.js
@@ -10,3 +10,30 @@ function pet(name){
 
 let myPet = pet('coco');
 console.log(myPet);
+
+
+
+function Pawn(pawn){
+    function getPawn(){
+        return pawn;
+    }
+    return getPawn();
+}
+
+function recursiveCall(arg){
+    const length = arg.length - 1;
+    console.log(arg.length);
+    return recursiveCallz(arg, length);
+}
+
+function recursiveCallz(arg, length){
+    if(length >= 0){
+        console.log(Pawn(arg[length]))
+        return recursiveCallz(arg, length - 1);
+    }else{
+        console.log("Thats all!");
+    }
+}
+
+var Marc = ["Jerry", "Joe", "Jimmy" , "Jimbob", "Marco", "Malinakus", "Jack", "Jane"];
+recursiveCall(Marc);
