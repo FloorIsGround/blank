46dfc6a9901a1d64ef684af5cf205bf76a6c61c8
FloorIsGround <112002787+FloorIsGround@users.noreply.github.com>
2022-09-19 11:47:57 -0500

    more examples

--- Jace's Stuff/JS Example 1/scope.js
+++ Jace's Stuff/JS Example 1/scope.js
@@ -38,3 +38,39 @@ function myFunc(){
 }
 
 myFunc();
+
+
+
+
+// NEW STUFF
+
+console.log(" ");
+console.log(" ");
+console.log(" ");
+
+var a = 5;
+let b = 6;
+const c = 7;
+//grave accents allow you to use variables within a string
+console.log(`global: ${a}`);
+console.log(`global: ${b}`);
+console.log(`global: ${c}`);
+
+testFunc();
+function testFunc(){
+    var a = 8;
+    let b = 9;
+    const c = 10;
+    console.log(`function scope: ${a}`);
+    console.log(`function scope: ${b}`);
+    console.log(`function scope: ${c}`);
+
+    {
+        var a = 11;
+        let b = 12;
+        const c = 13;
+        console.log(`local scope: ${a}`);
+        console.log(`local scope: ${b}`);
+        console.log(`local scope: ${c}`);
+    }
+}
